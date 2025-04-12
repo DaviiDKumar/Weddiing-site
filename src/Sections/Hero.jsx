131eea9d40bea912c595d4e0f72f8c91c9c7cdce
DaviDChoudharY <122169398+DaviDChoudharY@users.noreply.github.com>
2025-04-12 05:51:03 +0530

    💍 Initial commit: Wedding website setup

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -5,7 +5,8 @@ function Hero() {
     <section className="hero-bg">
       <div className="hero-overlay">
         <div className="hero-content">
-          <h2 className="hero-title">Niharika 💖 Kuldeep</h2>
+          <h2 className="hero-title">A Journey of Forever, Hand in Hand </h2>
+
           <p className="hero-subtitle">
             You are joyfully invited to celebrate the sacred union of love, laughter, and lifelong memories.
           </p>
